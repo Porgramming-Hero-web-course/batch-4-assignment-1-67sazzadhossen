094062deb56fc7a869139814e6112a05f5571607
67sazzadhossen <sazzadhossen010@gmail.com>
2024-11-08 11:07:10 +0600

    revised

--- src/problem-5.ts
+++ src/problem-5.ts
@@ -1,6 +1,6 @@
 // Write a generic function getProperty that takes an object and a property name as arguments and returns the property value. Add a constraint to ensure the property name exists on the object.
 
-function getProperty<T, K extends keyof T>(objct: T, type: K): T[K] {
+const getProperty = <T, K extends keyof T>(objct: T, type: K): T[K]=> {
   return objct[type];
 }
 
